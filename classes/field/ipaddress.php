b9eca9204eaacb295ff1d163a400fbefa5bf1a1c
Shawn Sprehe <memakeit@gmail.com>
2010-07-14 19:38:23 -0600

    updated comments

--- classes/field/ipaddress.php
+++ classes/field/ipaddress.php
@@ -4,7 +4,7 @@
 *
 * @package		Jelly
 * @author		Me Make It
-* @copyright	(c) 2009 Me Make It
+* @copyright	(c) 2010 Me Make It
 * @license		http://www.memakeit.com/license
 */
 class Field_IPAddress extends Field_Integer
