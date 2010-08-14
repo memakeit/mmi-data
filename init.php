f7072d71f85c6d8f750820bb100f2e05f0701113
Shawn Sprehe <memakeit@gmail.com>
2010-08-14 14:30:54 -0600

    updated routes

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 // Test routes
 if (Kohana::$environment !== Kohana::PRODUCTION)
 {
-	Route::set('mmi/data/test', 'mmi/data/test/<controller>(/<action>)', array('controller' => '.+'))
+	Route::set('mmi/data/test', 'mmi/data/test/<controller>(/<action>)')
 	->defaults(array
 	(
 		'directory' => 'mmi/data/test',
